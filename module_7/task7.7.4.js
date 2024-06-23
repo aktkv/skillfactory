a553882d5eac6b835535d487d4c4f84084effb43
Alina <alikotkova@gmail.com>
2024-06-23 16:04:46 +0600

    Модуль 7. Функции и объекты

--- module_7/task7.7.4.js
+++ module_7/task7.7.4.js
@@ -1,3 +1,19 @@
 // Создайте функцию setFullName, которая будет устанавливать свойство fullName у переданного ей объекта.
 //  Используйте метод bind, чтобы создать новую функцию setPersonFullName, которая будет устанавливать fullName для объекта person из предыдущего примера. 
 //  Вызовите setPersonFullName с параметром "John Smith" и убедитесь, что свойство fullName объекта person было изменено соответствующим образом.
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullName: ''
+};
+
+function setFullName(newFullName) {
+    this.fullName = newFullName;
+}
+
+const setPersonFullName = setFullName.bind(person);
+
+setPersonFullName("John Smith");
+
+console.log(person.fullName); 
