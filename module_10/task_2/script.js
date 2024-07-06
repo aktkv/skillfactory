7a05018ca9748f51c53744545a4fd368a2edc730
Alina <alikotkova@gmail.com>
2024-07-06 10:22:19 +0600

    10.5. Geolocation. Notification. Screen size

---
+++ module_10/task_2/script.js
@@ -0,0 +1,6 @@
+function getScreenSize() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  
+  alert(`Ширина экрана: ${screenWidth}px\nВысота экрана: ${screenHeight}px`);
+}
